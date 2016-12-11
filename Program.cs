87138cb0053b75218d67c2772ee576c56472961d
homirun <hatunemiku.moco@gmail.com>
2016-12-11 23:23:05 +0900

    Add files via upload
    最初のファイル

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ApplicationBridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("path.txt", Encoding.GetEncoding("Shift_JIS"));
+                string path = sr.ReadToEnd();
+                sr.Close();
+                System.Diagnostics.Process PS = System.Diagnostics.Process.Start(path);
+                PS.WaitForExit();
+            }
+            catch
+            {
+                Console.WriteLine("path.txtが存在しないか、指定されたパスが見つかりません。");
+                Console.WriteLine("Enterキーを押してください。");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
